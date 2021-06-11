a4775e1dff8634c2b25e21fbdd73a6dc1747242d
rgoldman100-bigboy <64619975+rgoldman100-bigboy@users.noreply.github.com>
2021-06-11 16:29:06 -0500

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@
 // })
 const fetchData = (location) => {
 
-    fetch('http://localhost:3000/Weather?address=' + location).then((response) => {
+    fetch('/Weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             messageOne.textContent = data.error
--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ console.log(__dirname)
 console.log(path.join(__dirname, '../public'))
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -118,6 +119,6 @@ app.get('*', (req, res) => {
 // app.com/weather
 
 // start the server up, for this example, we use the common development port 3000
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port + '.')
 })
